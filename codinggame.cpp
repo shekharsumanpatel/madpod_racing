5f0613af4d72cecbc28e7a47ffeab628d287ba77
ShekharSumanPatel <shekharsumanpatel@gmail.com>
2022-04-19 22:00:37 +0300

    print command for x y thrust values

--- codinggame.cpp
+++ codinggame.cpp
@@ -9,23 +9,32 @@ using namespace std;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+void printCommand(int next_checkpoint_x, int next_checkpoint_y, int thrust){
+    cout<< next_checkpoint_x<<" "<<next_checkpoint_y<<" "<<thrust<<endl;
 
+}
 int main()
 {
+    int x;
+    int y;
+    int next_checkpoint_x; // x position of the next check point
+    int next_checkpoint_y; // y position of the next check point
+    int next_checkpoint_dist; // distance to the next checkpoint
+    int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
+    cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
+    int opponent_x;
+    int opponent_y;
+    cin >> opponent_x >> opponent_y; cin.ignore();
+
+    printCommand(next_checkpoint_x,next_checkpoint_y,100);
+
 
     // game loop
     while (1) {
-        int x;
-        int y;
-        int next_checkpoint_x; // x position of the next check point
-        int next_checkpoint_y; // y position of the next check point
-        int next_checkpoint_dist; // distance to the next checkpoint
-        int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
         cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
-        int opponent_x;
-        int opponent_y;
         cin >> opponent_x >> opponent_y; cin.ignore();
-
+        printCommand(next_checkpoint_x,next_checkpoint_y,100);
+        
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
@@ -33,6 +42,6 @@ int main()
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100) or "BOOST" or "SHIELD"
         // i.e.: "x y thrust"
-        cout << next_checkpoint_x << " " << next_checkpoint_y << " 80" << endl;
+        
     }
 }
