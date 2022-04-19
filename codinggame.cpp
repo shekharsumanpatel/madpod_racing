c99e8669ee615292f20472d9af40dc0374c9f1fe
ShekharSumanPatel <shekharsumanpatel@gmail.com>
2022-04-19 22:16:33 +0300

    thrust based on angle function added

--- codinggame.cpp
+++ codinggame.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -11,6 +12,13 @@ using namespace std;
  **/
 void printCommand(int next_checkpoint_x, int next_checkpoint_y, int thrust){
     cout<< next_checkpoint_x<<" "<<next_checkpoint_y<<" "<<thrust<<endl;
+
+}
+
+int angleToThrust(int next_checkpoint_angle, int max_angle, int power_val){
+    int abs_angle = abs(next_checkpoint_angle);
+    float fract = 1 - pow(abs_angle/max_angle, power_val);
+    return (int)100*fract;
 
 }
 int main()
@@ -25,15 +33,27 @@ int main()
     int opponent_x;
     int opponent_y;
     cin >> opponent_x >> opponent_y; cin.ignore();
+    int thrust = 100;
 
-    printCommand(next_checkpoint_x,next_checkpoint_y,100);
+    printCommand(next_checkpoint_x,next_checkpoint_y,thrust);
 
 
     // game loop
     while (1) {
         cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
         cin >> opponent_x >> opponent_y; cin.ignore();
-        printCommand(next_checkpoint_x,next_checkpoint_y,100);
+
+        if(next_checkpoint_angle<90){
+            
+            thrust = angleToThrust(next_checkpoint_angle,90,3);
+            cerr<<" thrust val = "<<thrust<<endl;
+        }
+        else{
+            thrust = 0;
+            cerr<<" thrust val = "<<thrust<<endl;
+        }
+
+        printCommand(next_checkpoint_x,next_checkpoint_y,thrust);
         
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
