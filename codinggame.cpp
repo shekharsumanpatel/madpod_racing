e9c042dd434dc694067024f6044b377d871d56cb
ShekharSumanPatel <shekharsumanpatel@gmail.com>
2022-04-20 12:30:47 +0300

    Parameter tuning

--- codinggame.cpp
+++ codinggame.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <math.h>
 #include <set>
-#include<map>
+#include <map>
 
 using namespace std;
 
@@ -20,12 +20,25 @@ void printCommand(int next_checkpoint_x, int next_checkpoint_y, int thrust){
         cout<< next_checkpoint_x<<" "<<next_checkpoint_y<<" BOOST"<<endl;
         cerr<<" We are full on boosting"<<endl;
     }
+    else{
+        cout<< next_checkpoint_x<<" "<<next_checkpoint_y<<" SHIELD"<<endl;
+        cerr<<" ATTACK !!!"<<endl;
+    }
 }
 
 int angleToThrust(int next_checkpoint_angle, int max_angle, int power_val){
-    float fract = 1 - pow(next_checkpoint_angle/max_angle, power_val);
-    return (int)100*fract;
+    if(next_checkpoint_angle < 45){
+        return 100;
+    }
+    else{
+        float fract = 1 - pow(next_checkpoint_angle/max_angle, power_val);
+        return (int)100*fract;
+    }
+}
 
+int distToThrust(int dist_next_checkpoint, int dist_thresh, int power_val, int checkpoint_radius){
+    float fract = pow(((dist_next_checkpoint - checkpoint_radius)/(dist_thresh - checkpoint_radius)), power_val);
+    return (int)100*fract;
 }
 
 int calculateEuclideanDistance(int x_1, int y_1, int x_2, int y_2 ){
@@ -52,36 +65,44 @@ int main()
     int num_of_iter_max_dist = 7;
     int max_checkpoint_dist = pow(10,5);
     float fract_max_dist_boost_act = 0.75;
-    int angle_thresh_boost_act = 30;
+    int angle_thresh_boost_act = 15;
     map<int,int> map_val_angle_to_thrust;
+    map<int,int> map_val_dist_to_thrust;
     int max_angle_for_angle_thrust_calc = 90;
     int power_val_angle_thrust_calc = 3;
+    int min_dist_for_speed_up = 650;
+    int checkpoint_radius = 500;
     int abs_angle;
+    int dist_to_oppon = 0;
 
-    for(int i = 0; i < 90; i++){
+    for(int i = 0; i < max_angle_for_angle_thrust_calc; i++){
         map_val_angle_to_thrust[i] = angleToThrust(i,max_angle_for_angle_thrust_calc,power_val_angle_thrust_calc);
     }
+
+    // for(int i = checkpoint_radius - 10; i < min_dist_for_speed_up; i++){
+    //     map_val_dist_to_thrust[i] = distToThrust(i, min_dist_for_speed_up, power_val_angle_thrust_calc, checkpoint_radius - 10);
+    // }
 
     // game loop
     while (1) {
         cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
         cin >> opponent_x >> opponent_y; cin.ignore();
+        dist_to_oppon = calculateEuclideanDistance(x, y, opponent_x, opponent_y);
+        if(dist_to_oppon < 802){
+            thrust = -2;
+            printCommand(next_checkpoint_x,next_checkpoint_y,thrust);
+            break;
+        }
 
         abs_angle = abs(next_checkpoint_angle);
 
-        if(abs_angle<90){
+        if(abs_angle < max_angle_for_angle_thrust_calc ){
             thrust = map_val_angle_to_thrust[abs_angle];
-            cerr<<" thrust val = "<<thrust<<endl;
         }
         else{
             thrust = 0;
-            cerr<<" thrust val = "<<thrust<<endl;
         }
 
-        cerr<<" Bosst Value "<<boost<<endl;
-        cerr<<" count of iter"<<count_of_iter<<endl;
-        cerr<<"next_checkpoint_x"<<next_checkpoint_x<<endl;
-        cerr<<"next_checkpoint_y"<<next_checkpoint_y<<endl;
 
         if(boost == false){
             if(last_checkpoint_x != next_checkpoint_x || last_checkpoint_y != next_checkpoint_y){
@@ -101,16 +122,6 @@ int main()
 
         last_checkpoint_x = next_checkpoint_x;
         last_checkpoint_y = next_checkpoint_y;
-
-
-        
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100) or "BOOST" or "SHIELD"
-        // i.e.: "x y thrust"
         
     }
 }
